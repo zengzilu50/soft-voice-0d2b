b1005ba47356c62f723e1b706d077eb9cba71f96
zengzilu50 <zengzilu50@outlook.com>
2025-04-11 22:20:55 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Zengdashuai Blog";
-export const SITE_DESCRIPTION = "Welcome to Zengdashuai's website!";
+export const SITE_TITLE = "曾大帅的博客";
+export const SITE_DESCRIPTION = "欢迎来到曾大帅博客！";
